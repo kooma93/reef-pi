847d93da17a77b0c6c7d0e94f3b354a8b95f6ada
KweonMo Koo <mikekoo2010@gmail.com>
2019-06-22 21:20:00 -0700

    Add types for props

--- front-end/src/equipment/equipment_form.tsx
+++ front-end/src/equipment/equipment_form.tsx
@@ -1,10 +1,15 @@
 import EditEquipment from './edit_equipment'
 import EquipmentSchema from './equipment_schema.jsx'
 import { withFormik } from 'formik'
+import { outletsLoaded } from '../redux/actions/outlets';
 
 const EditEquipmentForm = withFormik({
   displayName: 'EquipmentForm',
-  mapPropsToValues: props => ({
+  mapPropsToValues: (props: {
+    equipment: object,
+    outlets: ,
+    actionLabel: string
+  }) => ({
     name: (props.equipment && props.equipment.name) || '',
     outlet: (props.equipment && props.equipment.outlet) || '',
     id: (props.equipment && props.equipment.id) || '',
